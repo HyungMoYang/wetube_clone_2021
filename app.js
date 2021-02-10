dec1d825f629d9fc53cfd98d6aea7f8ce3751dd3
TipsyBoy <gudah13@naver.com>
2021-02-10 18:05:53 +0900

    #6.3 Loggin the User In

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
+import passport from "passport";
 import cookieParser from "cookie-parser";
 import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+
+import "./passport";
 
 const app = express();
 
@@ -20,6 +23,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 // video 수정
 app.use((req, res, next) => {
   res.setHeader(
@@ -30,6 +36,7 @@ app.use((req, res, next) => {
 });
 
 app.use(localsMiddleware);
+
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
