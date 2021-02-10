9c5d74ded3ea2ad0152961977d4bc845c341f2e8
TipsyBoy <gudah13@naver.com>
2021-02-11 07:51:24 +0900

    #6.4 Sessions on Express

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import passport from "passport";
+import session from "express-session";
 import cookieParser from "cookie-parser";
 import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
@@ -23,6 +24,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
