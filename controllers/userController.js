94e76023542112d85a6c66b6c774b7bdca28cdf6
TipsyBoy <gudah13@naver.com>
2021-02-13 17:53:04 +0900

    #6.6 Github Log In part One

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,8 +39,14 @@ export const postLogin = passport.authenticate("local", {
   successRedirect: routes.home,
 });
 
+export const githubLoginCallback = (accessToken, refreshToken, profile, cb) => {
+  console.log(accessToken, refreshToken, profile, cb);
+};
+
+export const githubLogin = () => {};
+
 export const logout = (req, res) => {
-  // To Do: Process Logout
+  req.logout();
   res.redirect(routes.home);
 };
 
